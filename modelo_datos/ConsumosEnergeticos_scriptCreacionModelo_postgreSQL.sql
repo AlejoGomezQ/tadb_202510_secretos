14a3f489a40a84da49eb8dd8477e0d2b9db6ac82
Juan Dario Rodas M. <jdrodas@hotmail.com>
2025-03-06 07:34:41 -0500

    closes #1;  Realizado el abastecimiento de infraestructura para la capa de persistencia de datos

--- modelo_datos/ConsumosEnergeticos_scriptCreacionModelo_postgreSQL.sql
+++ modelo_datos/ConsumosEnergeticos_scriptCreacionModelo_postgreSQL.sql
@@ -22,32 +22,32 @@ docker run --name postgres-energia -e POSTGRES_PASSWORD=unaClav3 -d -p 5432:5432
 -- Con usuario Postgres:
 
 -- crear el esquema la base de datos
-create database energia_db;
+create database servicios_db;
 
 -- Conectarse a la base de datos
-\c energia_db;
+\c servicios_db;
 
 -- Creamos un esquema para almacenar todo el modelo de datos del dominio
 create schema core;
 
 -- crear el usuario con el que se implementará la creación del modelo
-create user energia_app with encrypted password 'unaClav3';
+create user servicios_app with encrypted password 'unaClav3';
 
 -- asignación de privilegios para el usuario
-grant connect on database energia_db to energia_app;
-grant create on database energia_db to energia_app;
-grant create, usage on schema core to energia_app;
-alter user energia_app set search_path to core;
+grant connect on database servicios_db to servicios_app;
+grant create on database servicios_db to servicios_app;
+grant create, usage on schema core to servicios_app;
+alter user servicios_app set search_path to core;
 
 -- crear el usuario con el que se conectará la aplicación
-create user energia_usr with encrypted password 'unaClav3';
+create user servicios_usr with encrypted password 'unaClav3';
 
 -- asignación de privilegios para el usuario
-grant connect on database energia_db to energia_usr;
-grant usage on schema core to energia_usr;
-alter default privileges for user energia_usr in schema core grant insert, update, delete, select on tables to energia_usr;
-alter default privileges for user energia_usr in schema core grant execute on routines TO energia_usr;
-alter user energia_usr set search_path to core;
+grant connect on database servicios_db to servicios_usr;
+grant usage on schema core to servicios_usr;
+alter default privileges for user servicios_usr in schema core grant insert, update, delete, select on tables to servicios_usr;
+alter default privileges for user servicios_usr in schema core grant execute on routines TO servicios_usr;
+alter user servicios_usr set search_path to core;
 
 -- Activar la extensión que permite el uso de UUID
 create extension if not exists "uuid-ossp";

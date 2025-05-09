004348ba32b4ec526a4269ba73e8ef352f5b9c44
AlejoGomezQ <alejandro.gomezq97@gmail.com>
2025-05-08 20:50:48 -0500

    feat: change conection string to use key vault

--- CONSUMOS_ENERGETICOS_CS_REST_SQL_API/CONSUMOS_ENERGETICOS_CS_REST_SQL_API/DbContexts/PgsqlDbContext.cs
+++ CONSUMOS_ENERGETICOS_CS_REST_SQL_API/CONSUMOS_ENERGETICOS_CS_REST_SQL_API/DbContexts/PgsqlDbContext.cs
@@ -1,15 +1,34 @@
 ﻿using Npgsql;
 using System.Data;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
 
 namespace CONSUMOS_ENERGETICOS_CS_REST_SQL_API.DbContexts
 {
     public class PgsqlDbContext(IConfiguration unaConfiguracion)
     {
-        private readonly string cadenaConexion = unaConfiguracion.GetConnectionString("ConsumosPL")!;
+        // private readonly string cadenaConexion = unaConfiguracion.GetConnectionString("ConsumosPL")!;
 
         public IDbConnection CreateConnection()
         {
-            return new NpgsqlConnection(cadenaConexion);
+            SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
+                {
+                    Delay= TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                }
+            };
+            var client = new SecretClient(new Uri("https://consumosenergetico.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+            KeyVaultSecret secret = client.GetSecret("conectionString");
+
+            string secretValue = secret.Value;
+
+            return new NpgsqlConnection(secretValue);
         }
     }
 }
